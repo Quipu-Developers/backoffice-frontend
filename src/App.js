167c6b7b923ff752b54430a9a13d227cc4822867
lhimjy <lhimjy1105@naver.com>
2024-06-30 23:28:10 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      <span className="Box">
+        <span className='Title_Quipu'>
+          Quipu DB
+        </span>
+        <span className='Menu_login'>
+          Login
+        </span>
+        id : <input className='input_id'>
+        </input>
+        password : <input className='input_password'>
+        </input>
+      </span>
     </div>
+    
   );
 }
 
