13b52e8051189c7dfcb91437465d97df66e13562
lhimjy <lhimjy1105@naver.com>
2024-07-01 17:04:31 +0900

    second commit

--- src/App.js
+++ src/App.js
@@ -3,20 +3,30 @@ import './App.css';
 function App() {
   return (
     <div className='container'>
-      <span className="Box">
-        <span className='Title_Quipu'>
-          Quipu DB
-        </span>
+      <header className='logo'>
+        Quipu DB
+      </header>
+      <body className="login">
         <span className='Menu_login'>
           Login
         </span>
-        id : <input className='input_id'>
-        </input>
-        password : <input className='input_password'>
-        </input>
-      </span>
+        <span className='id'>
+          id : <input className='input_id'>
+          </input>
+        </span>
+        <span className='password'>
+          password : <input className='input_password'>
+          </input>
+        </span>
+        <button className='button_signin'>
+            Sign In
+        </button>
+      </body>
+      <footer className='copyright'>
+        Copyright 2024.QUIPU.
+        All rights reserved.
+      </footer>
     </div>
-    
   );
 }
 
