0db4a51b9dc860b35147763001790605efb9cd64
lhimjy <lhimjy1105@naver.com>
2024-07-01 22:23:46 +0900

    first commit after merge

--- src/page/login.jsx
+++ src/page/login.jsx
@@ -3,19 +3,25 @@ import "../style/login.css";
 export default function Login() {
 
   return (
-    <div className="lg-container">
-      <header className="logo">Quipu DB</header>
-      <body className="login">
-        <span className="Menu_login">Login</span>
-        <span className="id">
-          id : <input className="input_id"></input>
+    <div className="lg_container">
+      <header className="lg_logo">
+        Quipu DB
+      </header>
+      <div className="lg_login">
+        <span className="lg_Menu_login">
+          Login
         </span>
-        <span className="password">
-          password : <input className="input_password"></input>
+        <span className="lg_id">
+          id : <input className="lg_input_id"></input>
         </span>
-        <button className="button_signin">Sign In</button>
-      </body>
-      <footer className="copyright">
+        <span className="lg_password">
+          password : <input className="lg_input_password"></input>
+        </span>
+        <button className="lg_button_signin">
+          Sign In
+        </button>
+      </div>
+      <footer className="lg_copyright">
         Copyright 2024.QUIPU. All rights reserved.
       </footer>
     </div>
