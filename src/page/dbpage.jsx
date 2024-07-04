4c1f6642dc1210adfc564ef2da0259ef8a17bcfb
unknown <seesnight@naver.com>
2024-07-04 16:36:16 +0900

    엑셀 내보내기 구현 중

--- src/page/dbpage.jsx
+++ src/page/dbpage.jsx
@@ -1,5 +1,33 @@
 import dummydata from "../dummy/dummy.json";
+import * as XLSX from 'xlsx';
+import React from 'react';
 import "../style/dbpage.css";
+// import fs from 'fs';
+
+function ExcelExporter() {
+  const exportToExcel = ()  => {
+    const filename = '퀴푸 지원 명단.xlsx'
+    const worksheet = XLSX.utils.json_to_sheet(dummydata);
+    const workbook = XLSX.utils.book_new();
+    XLSX.utils.book_append_sheet(workbook, worksheet, 'Sheet1');
+    XLSX.writeFile(workbook, 'data.xlsx');
+
+    // if (fs.existsSync(filename)) {
+    //   const existingWorkbook = XLSX.readFile(filename);
+    //   XLSX.utils.book_append_sheet(existingWorkbook, worksheet, 'Sheet1');
+    //   XLSX.writeFile(existingWorkbook, filename);
+    // }
+    // else {
+    //   XLSX.writeFile(workbook, filename);
+    // }
+  };
+
+  return (
+    <div>
+      <button onClick = {exportToExcel}>엑셀 파일로 내보내기</button>
+    </div>
+  )
+}
 
 export default function Dbpage() {
   return (
@@ -8,7 +36,7 @@ export default function Dbpage() {
       <div className="bottombox">
         <div className="buttonlist">
           <button>불러오기</button>
-          <button>액셀 파일로 내보내기</button>
+          <ExcelExporter />
         </div>
 
         <div className="dbbox">
