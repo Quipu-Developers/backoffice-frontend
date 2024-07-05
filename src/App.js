d428acdaf2ae9ea68f0021cb20ef9862f0de75f4
LeeYena <leeyena@192.168.nate.com>
2024-07-05 20:38:41 +0900

    Chore: index.html 셋팅

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
     <div className="container">
       <Router>
         <Routes>
-          <Route path={""} element={<Login />} />
+          <Route path={"/"} element={<Login />} />
           <Route path={"/dbpage"} element={<Dbpage />} />
         </Routes>
       </Router>
