f6d4bc8511405b3384472a63124520591f8364c8
lhimjy <lhimjy1105@naver.com>
2024-07-06 21:08:25 +0900

    feat : axios 구현 및 form 형식 적용

--- src/page/login.jsx
+++ src/page/login.jsx
@@ -6,7 +6,7 @@ import { BrowserRouter as Router,useNavigate,Link,Routes,Route } from 'react-rou
 
 export default function Login() {
 
-//id, password 입력저장
+//id, password 저장
 // let [id, setId] = useState('');
   let [password, setPassword] = useState('');
 
@@ -14,24 +14,38 @@ export default function Login() {
 
 // 로그인 처리함수
   const LoginFunc = (e) => {
-    e.preventDefault();
     // 버튼 누를 때마다 발생하는 리프레시 막기
-    // if(!id) {
+    e.preventDefault();
+    // if(!id) {                             --> id 미입력시 alert!
     //   return alert("enter id!");
     // }
     if(!password){
       return alert("enter password!")
     }
     else{
-      navigate('/dbpage')
-    }
-  }
+      let body ={
+        password : password
+      };
+      navigate('/dbpage');
+      // axios.post('Endpoint location', body)             -->  post endpoint를 백엔드에서 구현하면 사용
+      // .then((res) => {
+      //   console.log(res.data);
+      //   if(res.data.code === 200){
+      //     console.log("로그인");
+      //     navigate('/dbpage')
+      //   }
+      // })
+      // .catch((error) => {
+      //   console.log(error, "error");
+      // });
+      };
+    };
 // 로그인 시 엔터키로 넘어가는 경우를 위한 함수
   const handleKeyPress = (e) => {
     if (e.key === 'Enter'){
       LoginFunc(e);
-    }
-  }
+    };
+  };
   // 로고 누르면 색 변환 구현중.. input/button 사용하면 되긴 할듯..
   // const handleClick = (e) => {
   //   document.getElementById('lg_logo_Quipu').className += 'lg_logo_Quipu'
@@ -47,20 +61,22 @@ export default function Login() {
 
   return (
     <div className="lg_container">
-      <header className="lg_logo_Quipu" /*onClick = { (e) => handleClick(e) }*/>
+      <header className="lg_logo_Quipu" /*onClick = { (e) => handleClick(e) }  -->  클릭 시 색상변경 구현중 */>
         Quipu_DB
       </header>
       <div className="lg_box_login">
-        {/* <span className="lg_logo_login">
-          Login
-        </span> */}
-        {/* <span className="lg_id">
-          id : <input className="lg_input_id" type="text" value={id} onChange={(e) => {setId(e.target.value)}} onKeyDown={ (e) => handleKeyPress(e) }></input>
-        </span> */}
-        <span className="lg_password">
-          e n t e r<input className="lg_input_password" type="password" value={password} onChange={(e) => {setPassword(e.target.value)}} onKeyDown={ (e) => handleKeyPress(e) }></input>
-          p a s s w o r d
-        </span>
+        <form>
+          {/*<span className="lg_logo_login">   -->  id 입력코드, 필요에 의해 주석처리
+            Login
+          </span> */}
+          {/* <span className="lg_id">
+            id : <input className="lg_input_id" type="text" value={id} onChange={(e) => {setId(e.target.value)}} onKeyDown={ (e) => handleKeyPress(e) }></input>
+          </span> */}
+          <span className="lg_password">
+            e n t e r<input className="lg_input_password" id='password' type="password" value={password} onChange={(e) => {setPassword(e.target.value)}} onKeyDown={ (e) => handleKeyPress(e) }></input>
+            <label htmlFor="password">p a s s w o r d</label>
+          </span>
+        </form>
         {/* <button className="lg_button_signin" type="button" onClick={ (e) => LoginFunc(e) }>
           Sign In
         </button> */}
@@ -71,6 +87,3 @@ export default function Login() {
     </div>
   );
 }
-
-// 주석처리 부분은 id 또는 변경사항을 아직 지우기 이전
-// 또는 구현중인 기능
